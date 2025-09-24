c83ebab5ed7c2d162d79a31c8d24062f367e84fb
Monalisa Silva Bezerra <monalisasb91@gmail.com>
2025-09-24 12:19:38 -0300

    Update script.js

--- Weather/script.js
+++ Weather/script.js
@@ -7,7 +7,7 @@ document.querySelector(".busca").addEventListener("submit", async(event) => {
         clearInfo();
         showWarning('Loading...');
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${OPENWEATHER_API_KEY}&units=metric&lang=pt_br`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${OPENWEATHER_API_KEY}&units=metric&lang=en`;
         
         let results = await fetch(url);
         let json = await results.json();
